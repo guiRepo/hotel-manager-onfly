0a650caf995a03ff1ad557257c71dc05e36c78b1
Guilherme André de Melo <guilhermeanm95@hotmail.com>
2025-06-25 21:51:20 -0300

    Configuração do scss no projeto

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
+// vite.config.ts
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/global.scss";`,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
 })
