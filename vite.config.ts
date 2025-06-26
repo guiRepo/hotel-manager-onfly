941c7716f45b090e6472869b1a5ae06a0ffe0793
Guilherme André de Melo <guilhermeanm95@hotmail.com>
2025-06-25 22:34:14 -0300

    Separação de scss

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/styles/global.scss";`,
+        additionalData: `@import "@/styles/_variables.scss";`,
       },
     },
   },
